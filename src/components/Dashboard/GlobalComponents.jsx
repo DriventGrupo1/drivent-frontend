4742cebdc3fbf54931a71f656686a313725c5677
Marcus Dantas <129130733+omarcusdantas@users.noreply.github.com>
2023-10-18 16:30:41 -0300

    Checkout page (#2)
    * feat: add checkout_page main structure and global components to avoid code duplication
    
    * fix: change h2 to h1 for SEO strategy

---
+++ src/components/Dashboard/GlobalComponents.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const PageTitle = styled.h2`
+  font-family: 'Roboto', sans-serif;
+  font-size: 34px;
+  color: #000000;
+  margin-bottom: 40px;
+`;
+
+export const SectionTitle = styled.h3`
+  font-family: 'Roboto', sans-serif;
+  font-size: 20px;
+  color: #8e8e8e;
+  margin-bottom: 20px;
+  margin-top: 30px;
+`;
+
+export const PageButton = styled.button`
+  font-family: 'Roboto', sans-serif;
+  font-size: 14px;
+  color: #000000;
+  background-color: #e0e0e0;
+  box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  border: none;
+  padding: 10.5px 13px;
+  cursor: pointer;
+`;
