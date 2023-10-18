a285295ea02fe9bf12da5c2ee019b045f7cb6b87
MarcusDantas <marvinsd@gmail.com>
2023-10-18 19:24:22 -0300

    feat: add section to see selected ticket

--- src/pages/Dashboard/Checkout/index.jsx
+++ src/pages/Dashboard/Checkout/index.jsx
@@ -1,12 +1,54 @@
-import { PageTitle, SectionTitle, PageButton } from "../../../components/Dashboard/GlobalComponents";
+import { useContext } from 'react';
+import styled from 'styled-components';
+
+import UserTicketContext from '../../../contexts/UserTicketContext';
+import { PageTitle, SectionTitle, PageButton } from '../../../components/Dashboard/GlobalComponents';
 
 export default function Checkout() {
+  const { userTicket } = useContext(UserTicketContext);
+
+  function generateTicketText() {
+    if (userTicket?.TicketType.isRemote) {
+      return 'Online';
+    } else if (userTicket?.TicketType.includesHotel) {
+      return 'Presencial + Com Hotel';
+    }
+    return 'Presencial + Sem Hotel';
+  }
+
   return (
     <>
       <PageTitle>Ingresso e pagamento</PageTitle>
       <SectionTitle>Ingresso escolhido</SectionTitle>
-      <PageButton>FINALIZAR PAGAMENTO</PageButton>
+      <TicketInfo>
+        <p>{generateTicketText()}</p>
+        <span>R$ {userTicket?.TicketType.price}</span>
+      </TicketInfo>
       <SectionTitle>Pagamento</SectionTitle>
+      <PageButton>FINALIZAR PAGAMENTO</PageButton>
     </>
   );
 }
+
+const TicketInfo = styled.div`
+  width: 290px;
+  height: 108px;
+  background-color: #ffeed2;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  font-family: 'Roboto', sans-serif;
+  justify-content: center;
+  text-align: center;
+
+  p {
+    font-size: 16px;
+    color: #454545;
+    margin-bottom: 10px;
+  }
+
+  span {
+    font-size: 14px;
+    color: #898989;
+  }
+`;
