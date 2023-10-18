b06e880dfb0842260b5d788c06d4bb47a48d1c66
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-10-18 20:20:19 -0300

    refactor: refatorando o código e criando um componente para o erro

--- src/pages/Dashboard/Payment/index.jsx
+++ src/pages/Dashboard/Payment/index.jsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from "react";
 import useEnrollment from "../../../hooks/api/useEnrollment";
-import styled from "styled-components";
-import { Typography } from "@mui/material";
+import UsuarioSemInscricao from "../../../components/Payment/UsarioSemIncricao";
 
 export default function Payment() {
 
@@ -9,39 +7,9 @@ export default function Payment() {
   
   if(!enrollment) {
     return(
-    <ErrorContainer>
-      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
-      <MessageContainer>
-        <ErrorMessage>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</ErrorMessage>
-      </MessageContainer>
-    </ErrorContainer>
+    <UsuarioSemInscricao></UsuarioSemInscricao>
     )
   }
 
   return 'Pagamento: Em breve!';
 }
-
-const ErrorContainer = styled.div`
-  min-height: 100%;
-`
-
-const ErrorMessage = styled.div`
-  width: 450px;
-  height: 46px;
-  text-align: center;
-  color: #8E8E8E;
-  font-size: 20px;
-  font-weight: 400;
-`
-
-const MessageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 246px;
-`
-
-const StyledTypography = styled(Typography)`
-  margin-bottom: 20px!important;
-`;
