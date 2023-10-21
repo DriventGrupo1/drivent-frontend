9169b2b8e855c5abbaee750108d3419655332e02
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-10-21 20:42:29 -0300

    fix: hotels page (#22)

--- src/components/Hotel/HotelComponent.jsx
+++ src/components/Hotel/HotelComponent.jsx
@@ -1,106 +1,99 @@
-import styled from "styled-components"
-import useHotelById from "../../hooks/api/useHotelById"
-import { useEffect, useState } from "react"
+import styled from 'styled-components';
+import useHotelById from '../../hooks/api/useHotelById';
+import { useEffect, useState } from 'react';
 
-export default function HotelComponent(props){
+export default function HotelComponent(props) {
+  const { hotelInfo } = props;
+  const { hotelById, hotelByIdLoading, hotelByIdError } = useHotelById(hotelInfo.id);
+  const [roomTypes, setRoomTypes] = useState('');
+  const [availableRooms, setAvailableRooms] = useState(0);
 
-    const {hotelInfo} = props
-    const { getHotelById, hotelById} = useHotelById()
-    const [roomTypes, setRoomTypes] = useState("")
-    const [availableRooms, setAvailableRooms] = useState(0)
+  useEffect(() => {
+    async function getRooms() {
+      const newRooms = hotelById.Rooms;
+      if (newRooms.length === 0) {
+        setRoomTypes('-');
+        setAvailableRooms(0);
+      } else {
+        const types = [];
 
-    useEffect(()=>{
+        setAvailableRooms(retorno.available);
+        if (newRooms.find((element) => element.capacity === 1)) types.push('Single');
+        if (newRooms.find((element) => element.capacity === 2)) types.push('Double');
+        if (newRooms.find((element) => element.capacity === 3)) types.push('Triple');
 
+        setRoomTypes(types.join(', '));
+      }
+    }
+    if (!hotelByIdLoading) {
+      getRooms();
+    }
+  }, []);
 
-
-        async function getHotels (){
-            try {
-                const response = await getHotelById(hotelInfo.id)
-                return response
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-        async function getRooms (){
-            const retorno = await getHotels()
-            const newRooms = retorno.Rooms
-            if(newRooms.length === 0){
-                setRoomTypes("-")
-                setAvailableRooms(0)
-            }else{
-                const types = []
-        
-                setAvailableRooms(retorno.available)
-                if(newRooms.find((element)=> element.capacity === 1)) types.push("Single")
-                if(newRooms.find((element)=> element.capacity === 2)) types.push("Double")
-                if(newRooms.find((element)=> element.capacity === 3)) types.push("Triple")
-        
-                setRoomTypes(types.join(', '))
-            }
-        }
-
-        getRooms()
-    }, [])
-
-    return(
+  return (
+    <>
+      {hotelByIdError ? (
+        'erro'
+      ) : (
         <HotelContainer>
-            <img src={hotelInfo.image} alt="" />
-            <HotelInfo>
-                <HotelName>{hotelInfo.name}</HotelName>
-                <RoomTypes>Tipos de acomodação:</RoomTypes>
-                <Info>{roomTypes}</Info>
-                <VagasDisponiveis>Vagas disponíveis:</VagasDisponiveis>
-                <Info>{availableRooms}</Info>
-            </HotelInfo>
+          <img src={hotelInfo.image} alt="" />
+          <HotelInfo>
+            <HotelName>{hotelInfo.name}</HotelName>
+            <RoomTypes>Tipos de acomodação:</RoomTypes>
+            <Info>{roomTypes}</Info>
+            <VagasDisponiveis>Vagas disponíveis:</VagasDisponiveis>
+            <Info>{availableRooms}</Info>
+          </HotelInfo>
         </HotelContainer>
-    )
+      )}
+    </>
+  );
 }
 
 const HotelContainer = styled.div`
-    height: 264px;
-    width: 196px;
-    background-color: #EBEBEB;
-    border-radius: 10px;
-    padding-top: 16px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    img{
-        width: 168px;
-        height: 109px;
-        border-radius: 5px;
-    }
-    flex-shrink: 0;
-`
+  height: 264px;
+  width: 196px;
+  background-color: #ebebeb;
+  border-radius: 10px;
+  padding-top: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 168px;
+    height: 109px;
+    border-radius: 5px;
+  }
+  flex-shrink: 0;
+`;
 
 const HotelInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top: 10px;
-    width: 168px;
-    font-family: 'Roboto', sans-serif;
-    font-size: 12px;
-    color: #3C3C3C;
-`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  width: 168px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 12px;
+  color: #3c3c3c;
+`;
 
 const HotelName = styled.p`
-    color: #343434;
-    font-size: 20px;
-    font-weight: 400;
-`
+  color: #343434;
+  font-size: 20px;
+  font-weight: 400;
+`;
 
 const RoomTypes = styled.p`
-    margin-top: 10px;
-    font-weight: 700;
-`
+  margin-top: 10px;
+  font-weight: 700;
+`;
 
 const Info = styled.p`
-    margin-top: 2px;
-    font-weight: 400;
-`
+  margin-top: 2px;
+  font-weight: 400;
+`;
 
 const VagasDisponiveis = styled.p`
-    margin-top: 14px;
-    font-weight: 700;
-`
+  margin-top: 14px;
+  font-weight: 700;
+`;
