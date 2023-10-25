7f65bd1e595ab5a9aa4aa90602068e1598eb8c07
Fernando <127893244+FSacerdote@users.noreply.github.com>
2023-10-25 14:04:07 -0300

    feat: telas da página de atividades (#32)
    * feat: telas da página de atividades
    
    * feat: api de atividades

--- src/pages/Dashboard/Activities/index.jsx
+++ src/pages/Dashboard/Activities/index.jsx
@@ -1,3 +1,19 @@
+import { useContext } from "react";
+import UserTicketContext from "../../../contexts/UserTicketContext";
+import { PageTitle, SectionTitle } from "../../../components/Dashboard/GlobalComponents";
+import ErrorComponent from "../../../components/Dashboard/ErrorComponent";
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { userTicket, userTicketError } = useContext(UserTicketContext)
+
+  return (
+    <>
+      <PageTitle>Escolha de atividades</PageTitle>
+      {(!userTicket || userTicket.status === "RESERVED")?
+      <ErrorComponent errorMessage={"Você precisa ter confirmado pagamento antes de fazer a escolha de atividades"}></ErrorComponent>
+      :(userTicket.TicketType.isRemote)?
+      <ErrorComponent errorMessage={"Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades."}></ErrorComponent>
+      :<></>}
+    </>
+  )
 }
