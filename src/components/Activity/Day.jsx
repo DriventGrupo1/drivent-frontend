8a9940c457a82b89c5516b02eca02249af374ce5
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-10-25 17:34:23 -0300

    feat: renderização dos dias com atividades do evento

---
+++ src/components/Activity/Day.jsx
@@ -0,0 +1,30 @@
+import dayjs from "dayjs"
+import styled from "styled-components"
+
+export default function Day(props){
+    const {data} = props
+    const diasDaSemana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+
+    return(
+    <DayContainer>
+        {diasDaSemana[dayjs(data).day()]}, {dayjs(data).date()}/{dayjs(data).month() + 1}
+    </DayContainer>
+    )
+}
+
+const DayContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 131px;
+    height: 37px;
+    flex-shrink: 0;
+    border-radius: 4px;
+    background: #E0E0E0;
+    box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
+    color: #000;
+    text-align: center;
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+`
--- src/pages/Dashboard/Activities/index.jsx
+++ src/pages/Dashboard/Activities/index.jsx
@@ -2,9 +2,28 @@ import { useContext } from "react";
 import UserTicketContext from "../../../contexts/UserTicketContext";
 import { PageTitle, SectionTitle } from "../../../components/Dashboard/GlobalComponents";
 import ErrorComponent from "../../../components/Dashboard/ErrorComponent";
+import styled from "styled-components";
+import Day from "../../../components/Activity/Day";
+import EventInfoContext from "../../../contexts/EventInfoContext";
+import useActivtiesByEventId from "../../../hooks/api/useActivitiesByEventId";
+import dayjs from "dayjs";
 
 export default function Activities() {
   const { userTicket, userTicketError } = useContext(UserTicketContext)
+  const { eventInfo } = useContext(EventInfoContext)
+  const { activitiesByEventId, activitiesByEventIdError, activitiesByEventIdLoading } = useActivtiesByEventId(eventInfo.id)
+
+  const datas = []
+
+  if(!activitiesByEventIdLoading){
+    activitiesByEventId.forEach(element => {
+      if(!datas.includes(dayjs(element.date).format('YYYY-MM-DD'))) {
+        datas.push(dayjs(element.date).format('YYYY-MM-DD'))
+      }
+    });
+  }
+
+  console.log(datas)
 
   return (
     <>
@@ -13,7 +32,17 @@ export default function Activities() {
       <ErrorComponent errorMessage={"Você precisa ter confirmado pagamento antes de fazer a escolha de atividades"}></ErrorComponent>
       :(userTicket.TicketType.isRemote)?
       <ErrorComponent errorMessage={"Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades."}></ErrorComponent>
-      :<></>}
+      :(<>
+        <SectionTitle>Primeiro, filtre pelo dia do evento:</SectionTitle>
+        <DaysContainer>
+          {datas.map((element, index)=><Day key={index} data={element}></Day>)}
+        </DaysContainer>
+      </>)}
     </>
   )
 }
+
+const DaysContainer = styled.div`
+  display: flex;
+  gap: 0 17px;
+`
